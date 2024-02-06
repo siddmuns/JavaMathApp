56e8b95ffa05bbfdb1659891fab0f8d433e27ab0
Munsif Siddiqui <siddmuns@sheridancollege.ca>
2024-02-06 10:29:42 -0500

    Added Rest of the features for the code

--- src/main/java/ca/sheridancollege/siddmuns/quiz1/controller/TableController.java
+++ src/main/java/ca/sheridancollege/siddmuns/quiz1/controller/TableController.java
@@ -1,4 +1,60 @@
 package ca.sheridancollege.siddmuns.quiz1.controller;
 
+import ca.sheridancollege.siddmuns.quiz1.model.Table;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
 public class TableController {
+    private final Logger log = LoggerFactory.getLogger(TableController.class);
+
+    private final Table table = new Table();
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        log.trace("index() is called");
+        List<Integer> tableRows = table.getTable();
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("tableRows", tableRows);
+        return modelAndView;
+    }
+
+    @PostMapping("/multiplyTable")
+    public ModelAndView multiplyTable(@RequestParam("value") int value) {
+        log.trace("multiplyTable() is called with: {}", value);
+        table.multiplyRows(value);
+        List<Integer> tableRows = table.getTable();
+        ModelAndView modelAndView = new ModelAndView("multiply");
+        modelAndView.addObject("tableRows",tableRows);
+        return modelAndView; // Redirect after post to prevent double submission
+    }
+
+    @GetMapping("/multiply")
+    public ModelAndView multiply() {
+        log.trace("multiply() is called");
+        List<Integer> tableRows = table.getTable();
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("tableRows", tableRows);
+        return modelAndView;
+    }
+
+    @GetMapping("/newIndex")
+    public ModelAndView newIndex() {
+        log.trace("newIndex() is called");
+        Table newTable = new Table();
+        List<Integer> tableRows = newTable.getTable();
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("tableRows", tableRows);
+        return modelAndView;
+    }
+
+
 }
--- src/main/java/ca/sheridancollege/siddmuns/quiz1/model/Table.java
+++ src/main/java/ca/sheridancollege/siddmuns/quiz1/model/Table.java
@@ -1,4 +1,30 @@
 package ca.sheridancollege.siddmuns.quiz1.model;
 
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Getter
 public class Table {
+    private List<Integer> table;
+
+    public Table() {
+        this.table = new ArrayList<>();
+        generateRandomValues();
+    }
+
+    public void multiplyRows(int n) {
+        table.replaceAll(element -> element * n);
+    }
+
+    public void generateRandomValues() {
+        table.clear();
+        Random random = new Random();
+        int numberOfRows = random.nextInt(1,10);
+        for (int i = 0; i < numberOfRows; i++) {
+            table.add(random.nextInt(1,10));
+        }
+    }
 }
